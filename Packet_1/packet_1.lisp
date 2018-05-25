1485024a9688798b846f8809cb4608f9e250d820
GaboGomez09 <gomezgabriel09@gmail.com>
2018-05-25 06:14:37 -0500

    Resolved part one

---
+++ Packet_1/packet_1.lisp
@@ -0,0 +1,33 @@
+#||========================================================================
+
+packet_1.lisp 
+
+Solutions for first packet of excersizes. Wish me luck.
+
+Author: Gabriel Gómez Téllez
+
+=========================================================================||#
+
+;;; 1)
+
+;; a)
+
+(nth 4 '(((1 2) 3) 4 (5 (6)) A (B C) D (E (F G))))
+
+;; b)
+
+;Given that 
+	; A leap year has 366 days 
+	; A day has 24 hours
+	; An hour has 60 minutes
+	; A minute has 60 seconds
+(* 366 24 60 60) 
+
+;; c)
+
+(and (not (= x 0)) (<= x y))
+
+;; d)
+
+(list ( / ( + -7.0 (sqrt (- (expt 7 2 ) ( * 4 2 5 )) ) ) (* 2 2) ) ( / ( - -7.0 (sqrt (- (expt 7 2 ) ( * 4 2 5 )) ) ) (* 2 2) ) )
+
