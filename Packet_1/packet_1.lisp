a29cc9aa7a3f01dc1511db43c32c9bf49f575eb5
GaboGomez09 <gomezgabriel09@gmail.com>
2018-05-26 16:57:22 -0500

    Completed part three

--- Packet_1/packet_1.lisp
+++ Packet_1/packet_1.lisp
@@ -44,3 +44,46 @@ Author: Gabriel Gómez Téllez
 ;; c)
 
 (sqrt (/ (+ (* -1 (- -4 3/8)) 1.4502) (expt -1 (expt (- 3 5) 1/3))))
+
+;; d)
+
+(expt (/ (expt (/ 65.402 (sqrt -1)) 1/5) 0.17) 1/7)
+
+;;; 3)
+#||
+a) (cdar '((one two) three four)))
+
+	Salida: Error de paréntesis
+	
+b) (append (cons '(eva lisa) '(karl sven)) '(eva lisa) '(karl sven))
+
+	Salida: ((eva lisa) karl sven eva lisa karl sven)
+
+c) (subst 'gitan 'birgitta '(eva birgitta lisa birgitta karin))
+
+	Salida: (eva gitan lisa gitan karin)
+
+d) (remove 'sven '(eva sven lisa sven anna))
+	
+	Salida: (eva lisa anna)
+
+e) (butlast '(karl adam nilsson gregg alisson vilma) 3)
+
+	Salida: (karl adam nilsson)
+
+f) (nth 2 '(a b c d e))
+	
+	Salida: C
+
+g) (nthcdr 2 '(a b c d e))  CDR es equivalente a Rest
+
+	Salida: (c d e)
+
+h) (intersection '(a b c) '(x b z c))
+
+	Salida: (c b)
+
+i) (cdadar '( ( ( ( (1 2 3) z) y) (x 4) ) 7 8 (a b c (5 (6 7 8) ) ) ) 	 	 )
+	
+	Salida: (4)
+||#
